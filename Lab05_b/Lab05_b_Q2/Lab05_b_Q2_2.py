873e8a87b74eba28b640596e90bcbf1726868626
FinnVAL <115410106+FinnVAL@users.noreply.github.com>
2024-03-01 22:05:18 +0700

    f

--- Lab05_b/Lab05_b_Q2/Lab05_b_Q2_2.py
+++ Lab05_b/Lab05_b_Q2/Lab05_b_Q2_2.py
@@ -5,8 +5,5 @@ class Vehicle:
         self.max_speed = max_speed
         self.mileage = mileage
 
-class Bus(Vehicle):
-    pass
-
-my_bus = Bus("School Volvo", 180, 12)
-print(f"Vehicle Name: {my_bus.name} Speed: {my_bus.max_speed} Mileage: {my_bus.mileage}")
+    def seating_capacity(self, capacity):
+        result f"The seating  capacity of a {self.name} is {capacity} passengers "
